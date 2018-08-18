80d27f48fecbc8330a03962617a8fe2f01eee3f3
huyangyang <13621099447@163.com>
2018-08-18 16:19:48 +0800

    add downloads

--- js/app.js
+++ js/app.js
@@ -1,23 +1,23 @@
 // get source data
-var getData = function(language, text) {
+var getData = function (language, text) {
     $.get('language.json', function (data) {
-       if (window.localStorage){
-           var storage = window.localStorage
-           storage.language = language  //storage language
-           storage.text = text //storage text
-       }
-       $("*[data-item]").each(function () {
-           var key = $(this).data('item')
-           if (data[key][language]) {
-               $(this).text(data[key][language])
-           }
-       })
-   })
+        if (window.localStorage) {
+            var storage = window.localStorage
+            storage.language = language  //storage language
+            storage.text = text //storage text
+        }
+        $("*[data-item]").each(function () {
+            var key = $(this).data('item')
+            if (data[key][language]) {
+                $(this).text(data[key][language])
+            }
+        })
+    })
 }
 
 $(document).ready(function () {
     // judge last choices
-    if (window.localStorage){
+    if (window.localStorage) {
         var storage = window.localStorage
         var language = storage.language || 'zh'
         var text = storage.text || '简体中文'
@@ -26,14 +26,23 @@ $(document).ready(function () {
         language == 'zh' ? $('#news_actions').attr("href", "https://www.jianshu.com/c/c130aec4a7d1") : $('#news_actions').attr("href", "https://medium.com/@wormholecash")
         getData(language, text)
     }
-    
-    
-    $('#chooseLanguage').click(function () { 
+
+    //toogle languages
+    $('#chooseLanguage').click(function () {
         $('#languageOptions').show()
+    }).mouseleave(function () {
+        $('#languageOptions').hide()
     })
-  
-    // choose language
-    $('.js-option').click(function (e) { 
+
+    //toogle downloads
+    $('#chooseDownload').click(function () {
+        $('#downloadOptions').show()
+    }).mouseleave(function () {
+        $('#downloadOptions').hide()
+    })
+
+    // choose languages
+    $('.js-option').click(function (e) {
         var text = $(this).text()
         e.stopPropagation() //stop propagation
         $('#chooseLanguage a').text(text)
