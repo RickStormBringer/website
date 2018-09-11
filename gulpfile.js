9b80efe429d3142e35d2a21ce518505420058d2e
huyangyang <13621099447@163.com>
2018-09-11 16:32:35 +0800

    update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -9,28 +9,15 @@ var gulp = require('gulp'),
     cssMin = require('gulp-csso'),
     uglify = require('gulp-uglify'),
     clearAll = require('del'),
-    runSequence = require('run-sequence'),
     imagemin = require('gulp-imagemin');
 
 
-gulp.task('default', ['clearAll', 'dev']);
-
-// entry
-gulp.task('dev', function (done) {
-    runSequence(
-        ['clearAll'],
-        ['revCss'],
-        ['revJs'],
-        ['revFonts'],
-        ['revImg'],
-        ['revIcons'],
-        ['revHead'],
-        ['revBody'],
-        ['revAutoHtml'],
-        done);
-});
+gulp.task('default', gulp.series(clear, revCss, revJs, revFonts, revImg, revIcons, revHead, revBody, revAutoHtml, function (done) {
+    done()
+}));
 
 // paths
+
 var cssSrc = ['./public/css/*.css', './public/css/*.css'],
     jsSrc = ['./public/js/*.js', './public/js/app.js'],
     headSrc = './views/partial/*.ejs',
@@ -41,67 +28,72 @@ var cssSrc = ['./public/css/*.css', './public/css/*.css'],
     fontsSrc = './public/fonts/*';
 
 // revAutoHtml
-gulp.task('revAutoHtml', function () {
+
+function revAutoHtml() {
     return gulp.src(autoHtmlSrc)
         .pipe(gulp.dest('./dist'));
-});
+}
+
 // css build
-gulp.task('revCss', function () {
+
+function revCss() {
     return gulp.src(cssSrc)
         .pipe(cssMin())
         .pipe(rev())
         .pipe(gulp.dest('./dist/css'))
         .pipe(rev.manifest())
         .pipe(gulp.dest('./dist/css'));
-});
-
+}
 
 // js build
-gulp.task('revJs', function () {
+
+function revJs() {
     return gulp.src(jsSrc)
         .pipe(uglify())
         .pipe(rev())
         .pipe(gulp.dest('./dist/js'))
         .pipe(rev.manifest())
         .pipe(gulp.dest('./dist/js'));
-});
+}
 
 // fonts
-gulp.task('revFonts', function () {
+
+function revFonts() {
     return gulp.src(fontsSrc)
         .pipe(gulp.dest('./dist/fonts'));
-});
+}
 
 // revImg
-gulp.task('revImg', function () {
+function revImg() {
     return gulp.src(imgSrc)
         .pipe(imagemin())
         .pipe(gulp.dest('./dist/images'));
-});
+}
+
 // revIcons
-gulp.task('revIcons', function () {
+
+function revIcons() {
     return gulp.src(iconsSrc)
         .pipe(imagemin())
         .pipe(gulp.dest('./dist/icons'));
-});
+}
 
 // replace path
-gulp.task('revHead', function () {
+
+function revHead() {
     return gulp.src(['dist/**/*.json', headSrc])
         .pipe(revC({
             replaceReved: true
         }))
         .pipe(gulp.dest('dist/views/partial'));
-});
+}
 
-gulp.task('revBody', function () {
+function revBody() {
     return gulp.src(['dist/**/*.json', bodySrc])
         .pipe(revC({replaceReved: true}))
         .pipe(gulp.dest('dist/views'));
-});
+}
 
-
-// clear dist
-gulp.task('clearAll', function () {
-    clearAll('./dist/*');
-});
+function clear() {
+    return clearAll('./dist/*');
+}
